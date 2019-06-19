2701c75f87b4b997afa3aed2f5db11c0e2de92fd
Tor Arne Falk <tor.arne.falk@gmail.com>
2019-06-20 00:36:38 +0200

    Created pages and components, fixed routing between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { GrasstypepageComponent } from './pages/grasstypepage/grasstypepage.component';
+import { ContactpageComponent } from './pages/contactpage/contactpage.component';
+import { SpecificpageComponent } from './pages/specificpage/specificpage.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: 'grasstypes', component: GrasstypepageComponent },
+  { path: 'contact', component: ContactpageComponent },
+  { path: 'specificpage', component: SpecificpageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PagenotfoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { SpecificpageComponent } from './pages/specificpage/specificpage.component';
+import { GrasstypepageComponent } from './pages/grasstypepage/grasstypepage.component';
+import { ContactpageComponent } from './pages/contactpage/contactpage.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SpecificpageComponent,
+    GrasstypepageComponent,
+    ContactpageComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
