8a77a698a36ad53ee5b3ed027906f9418e05c245
Tor Arne Falk <tor.arne.falk@gmail.com>
2019-06-21 01:39:28 +0200

    Made filter function that filters through cards array

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -7,14 +7,39 @@ import { FetchapiService } from '../../services/fetchapi.service';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+
   private apiUrl = "https://api.pokemontcg.io/v1/cards?setCode=base1";
-  pokemons: Array<any>;
+  specificUrl: string;
+  pokemonCards: Array<any>;
+  filteredCards: Array<any>;
 
   constructor(private fetchApiService: FetchapiService) { }
 
   ngOnInit() {
     this.fetchApiService.getPokemons(this.apiUrl)
-      .subscribe(data => {this.pokemons = data.cards});
-      // .subscribe(data => {console.log(data.cards)});
+      .subscribe(data => {
+        this.pokemonCards = data.cards;
+        this.filteredCards = data.cards;
+      });
+  }
+
+  keyPress(event: any) {
+    // this.pokemonCards = this.filteredCards.filter(str => {
+    //   return str.name.toLowerCase().includes(event.target.value);
+    // })
+    console.log(this.pokemonCards);
+  }
+
+  showCards(value: any){
+    event.preventDefault();
+    this.pokemonCards = this.filteredCards.filter(str => {
+      return str.name.toLowerCase().includes(value);
+    })
+    console.log(value)
+  }
+
+  specificCard(value: any){
+    this.specificUrl = "https://api.pokemontcg.io/v1/cards/" + value;
+    console.log(this.specificUrl);
   }
 }
